7e3cd40028cc9045b0d1c329bb711b05879ff874
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-04-05 14:51:26 +0300

    fix root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,31 @@
-import Providers from "@/components/Providers";
+import "./globals.css";
+import React from "react";
+
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <QueryClientProvider client={queryClient}>
+          <Header />
+
+          {children}
+
+          {modal}
+
+          <Footer />
+        </QueryClientProvider>
       </body>
     </html>
   );
