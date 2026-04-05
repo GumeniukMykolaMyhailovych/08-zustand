1666368414fa3ba3d386ac16b5c9058efd1deff9
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-04-05 08:54:45 +0300

    fix syntax modal + filter page

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,15 +1,5 @@
 import NotePreview from "./NotePreview.client";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-
-  return <NotePreview id={id} />;
-}import NotePreview from "./NotePreview.client";
-
 export default function Page({
   params,
 }: {
