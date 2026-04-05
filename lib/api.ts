308a6012eb1013c29df0ff345e378d1fb527e51c
Mykola Gumeniuk <gumeniuk.mykola@gmail.com>
2026-04-05 06:31:13 +0300

    fix: clean api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = "https://notehub-public.goit.study/api";
+
 export const fetchNotes = async (page = 1, tag?: string) => {
   const params: Record<string, string | number> = {
     page,
@@ -10,15 +12,34 @@ export const fetchNotes = async (page = 1, tag?: string) => {
     params.tag = tag;
   }
 
-  const { data } = await axios.get(
-    "https://notehub-public.goit.study/api/notes",
-    {
-      params,
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-      },
-    }
-  );
+  const { data } = await axios.get(`${BASE_URL}/notes`, {
+    params,
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+    },
+  });
 
   return data;
+};
+
+export const createNote = async (note: {
+  title: string;
+  content: string;
+  tag: string;
+}) => {
+  const { data } = await axios.post(`${BASE_URL}/notes`, note, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+    },
+  });
+
+  return data;
+};
+
+export const deleteNote = async (id: string) => {
+  await axios.delete(`${BASE_URL}/notes/${id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+    },
+  });
 };
